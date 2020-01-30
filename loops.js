e38700918a189e2ff1db7e9dfa86b56c0999e10a
Dyealex <dyealex@hughes.net>
2020-01-30 12:26:11 -0500

    added text

---
+++ loops.js
@@ -0,0 +1,58 @@
+while(false) {
+    console.log("hello");
+}
+
+var count = 15;
+var doCount = 15;
+
+
+function print (){
+    while(count >10){
+        console.log("test");
+        count--;
+    }
+do {
+    console.log("hiiiii");
+    doCount--;
+} while(doCount>20);
+count=200;
+}
+console.log(count);
+function prettyPrint(){
+    for(var i = 0; i<10; i++){
+        console.log("more testing");
+    }
+}
+// count increases
+// decreases count
+function prettyPrint(){
+    for(var i = 0; i<10; i--){
+        console.log("more testing");
+    }
+}
+function 
+printName(firstName, lastName){
+    console.log(firstName, lastName)
+}
+// function addTheme(a, b){
+//     return a*b;
+// }
+// Morgan said DO NOT do the above but I have no idea why
+var n = 0;
+var x = 0;
+
+while (n < 3) {
+  n++;
+  x += n;
+}
+// copy/pasted from MDN
+function myFunction(name){
+    return "hello + name"
+}
+// len and wid are placeholders that are waiting for values
+function areaRect (len, wid) {
+    var area;
+    area = len * wid; 
+    return area;
+}
+// example len=5 wid=10 5X10=500 areaRect=500?
