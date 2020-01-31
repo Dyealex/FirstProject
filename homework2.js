81cbd2acc336f54335dc553d7240e41512f71d51
Dyealex <dyealex@hughes.net>
2020-01-31 14:45:33 -0500

    added text

---
+++ homework2.js
@@ -0,0 +1,10 @@
+alert ("Hi there!");
+
+var counter = 1;
+while (counter <50) {
+    console.log(counter)
+counter + 2;
+// if (counter%2===0) {
+//     console.log(counter);
+}
+// }
---
+++ januaryThirtyOne.js
@@ -0,0 +1,13 @@
+let single = 'text in here'
+let double = "text in here"
+let backtick = `text in here` 
+// if you use backticks the enters and tabs show up. When you use quotes, they do not. However you type backticks will show up so be aware of how you type.
+var str = "I\'m an ironman!"
+var len = str.length
+console.log(str[0]);
+console.log(str[1]);
+console.log(length);
+for (var i = 0; i < str.length;i++) {
+    console.log(str[i]);
+}
+// this will run for the length of the string until it reaches the end of the string length. My string length is 14.
