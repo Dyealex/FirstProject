cc6de07d9803726794dec6983cc6aeffc5c2a44f
Dyealex <dyealex@hughes.net>
2020-02-03 12:00:51 -0500

    initial commit

---
+++ araymethod.js
@@ -0,0 +1,9 @@
+var clothes = ["shirt", "pants", "socks", "shoes"];
+for( var i = 0 < clothes.length; i++;) {
+console.log(clothes[i]);
+}
+array1.forEach(element => console.log(element));
+for(var i = 0; i < clothes.length; i++) {
+
+}
+var cars = new array (list of items)
---
+++ retryhomework.js
@@ -0,0 +1,20 @@
+// var randomNumber = 5;
+// randomNumber = 5;
+// var myGuess; 
+// myGuess = ("");
+//   if (myGuess == randomNumber) {
+//       console.log("Way to go!");
+//   } else {
+//       console.log("Try again...");
+//   }
+// commented the above out. copy/pasted this to codepen to get it to work. it does now but not quite right.
+  var randomNumber = 5;
+randomNumber = 5;
+var myGuess; 
+myGuess = ("5");
+  if (myGuess == randomNumber) {
+      console.log("Way to go!");
+  } else {
+      console.log("Try again...");
+  }
+// this works but not quite right.
